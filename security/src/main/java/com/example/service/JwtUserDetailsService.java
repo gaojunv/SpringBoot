f5b28b818705d4e5e133a945e90f4fb546b588b7
gaojun <gaojun@fcsc.com>
2017-02-07 17:13:38 +0800

    修复BUG

--- security/src/main/java/com/example/service/JwtUserDetailsService.java
+++ security/src/main/java/com/example/service/JwtUserDetailsService.java
@@ -2,7 +2,6 @@ package com.example.service;
 
 import com.example.JwtUserFactory;
 import com.example.model.User;
-import com.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
